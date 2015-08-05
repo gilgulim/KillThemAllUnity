ff96aadf446871fb49f9a1ad1bc4f5d4f9370468
gilgulim <gilperetz2002@gmail.com>
2015-08-05 22:32:15 +0300

    Added script folder. started working on gameplay and coliision detection

---
+++ FinalProject/Assets/Scripts/ObjectDetectionScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	void OnCollisionEnter (Collision col)
+	{
+		Debug.Log("Collision detected.");
+		if(col.gameObject.tag == "AmmoObject")
+		{
+			Destroy(col.gameObject);
+			//GameObject.Find("AmmoAmount").guiText = "60/60";
+			Debug.Log("Refill Ammo.");
+		}else if((col.gameObject.tag == "HealthObject"))
+		{
+			//GameObject.Find("HealthAmount").guiText.text = "100/100";
+			Debug.Log("Refill Health.");
+		}
+		Destroy(col.gameObject);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
